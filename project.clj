2b9c4c1aa7f70296b231421aec6a30d9a6777224
rinuboney <rinuboney@gmail.com>
2014-09-10 11:23:50 +0530

    Added clatern.io

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [net.mikera/core.matrix "0.29.1"]])
+                 [net.mikera/core.matrix "0.29.1"]
+                 [org.clojure/data.csv "0.1.2"]])
---
+++ src/clatern/io.clj
@@ -0,0 +1,22 @@
+(ns clatern.io
+  (:require [clojure.core.matrix :refer :all]
+            [clojure.core.matrix.dataset :refer :all]
+            [clojure.data.csv :as csv]
+            [clojure.java.io :as io]))
+
+(defn load-data [filename]
+  (matrix (with-open [in-file (io/reader filename)]
+            (doall
+             (csv/read-csv in-file)))))
+
+(defn load-dataset [filename]
+  (let [data (load-data filename)]
+    (dataset (first data) (rest data))))
+
+(defn save-data [filename data]
+  (with-open [out-file (io/writer filename)]
+    (csv/write-csv out-file data)))
+
+(defn save-dataset [filename data]
+  (let [data (cons (:column-names data) (transpose (:columns data)))]
+    (save-data filename data)))
