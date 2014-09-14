469d2a30e076d75a9327a2f7f9e1579daf619bfe
rinuboney <rinuboney@gmail.com>
2014-09-14 15:10:38 +0530

    added min max scaling

--- src/clatern/preprocessing.clj
+++ src/clatern/preprocessing.clj
@@ -6,16 +6,14 @@
 
 ; API
 
-(defmulti set-options (fn [model options coder] coder))
 (defmulti fit (fn [model data coder] coder))
 (defmulti encode (fn [model data coder] coder))
 
-; Mean Normalization
-
-(defmethod set-options :mean-normalization
+(defn set-options
   [model options coder]
-  (assoc model :preprocessing (assoc (:preprocessing model) :mean-normalization options)))
+  (assoc model :preprocessing (assoc (:preprocessing model) coder options)))
 
+; Mean Normalization
 (defmethod fit :mean-normalization
   [model data coder]
   (let [metadata (:mean-normalization (:preprocessing model))
@@ -24,13 +22,10 @@
         mu (mean cols)
         std (sd cols)
         metadata (merge metadata {:mean mu, :sd std})]
-    (assoc model :preprocessing (assoc (:preprocessing model) :mean-normalization metadata))))
+    (assoc model :preprocessing (assoc (:preprocessing model) coder metadata))))
 
 (defn- mean-norm-arg [X mu sd]
   (M// (M/- X mu) sd))
-
-(defn- mean-denorm-arg [X mu sd]
-  (M/+ mu (M/* sd X)))
 
 (defmethod encode :mean-normalization
   [model data coder]
@@ -43,3 +38,29 @@
                                       (rows cols)))]
     (merge-datasets (except-columns data col-names)
                     (dataset col-names normalized))))
+
+; Min Max Scaling
+
+(defn- min-max-scale [X min max]
+  (M// (M/- X min) (M/- max min)))
+
+(defmethod fit :min-max-scaling
+  [model data coder]
+  (let [metadata (:min-max-scaling (:preprocessing model))
+        column-names (:column-names metadata)
+        cols (transpose (:columns (select-columns data column-names)))
+        cmin (or (:min metadata) (map #(apply min %) (columns cols)))
+        cmax (or (:max metadata) (map #(apply max %) (columns cols)))
+        metadata (merge metadata {:min cmin, :max cmax})]
+    (assoc model :preprocessing (assoc (:preprocessing model) coder metadata))))
+
+(defmethod encode :min-max-scaling
+  [model data coder]
+  (let [metadata (:min-max-scaling (:preprocessing model))
+        column-names (:column-names metadata)
+        cols (transpose (:columns (select-columns data column-names)))
+        cmin (:min metadata)
+        cmax (:max metadata)
+        scaled (matrix (map #(min-max-scale % cmin cmax) (rows cols)))]
+    (merge-datasets (except-columns data column-names)
+                    (dataset column-names scaled))))
