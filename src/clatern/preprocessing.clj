4b45b567f0d96fc68ed190d6262584215625e702
rinuboney <rinuboney@gmail.com>
2014-12-08 20:40:39 +0530

    updated encoders

--- src/clatern/preprocessing.clj
+++ src/clatern/preprocessing.clj
@@ -6,12 +6,12 @@
 
 (defn mean-normalizer
   "Create a mean normalization encoder from the dataset/matrix ds for the specified indices"
-  [ds indices]
-  (if (= :all indices)
-    (let [mu (mean ds)
+  ([ds]
+     (let [mu (mean ds)
           std (sd ds)]
-      (fn [v] (div (sub v mu) std)))
-    (let [ds' (transpose (mapv #(get-column ds %) indices))
+      (fn [v] (div (sub v mu) std))))
+  ([ds indices]
+     (let [ds' (transpose (mapv #(get-column ds %) indices))
           mu (mean ds')
           std (sd ds')]
       (fn [v]
@@ -21,12 +21,12 @@
 
 (defn min-max-scaler
   "Create a min max scaling encoder from the dataset/matrix ds for the specified indices"
-  [ds indices]
-  (if (= :all indices)
-    (let [mins (map emin (columns ds))
+  ([ds]
+     (let [mins (map emin (columns ds))
           maxs (map emax (columns ds))]
-      (fn [v] (div (sub v mins) (sub maxs mins))))
-    (let [ds' (transpose (mapv #(get-column ds %) indices))
+      (fn [v] (div (sub v mins) (sub maxs mins)))))
+  ([ds indices]
+     (let [ds' (transpose (mapv #(get-column ds %) indices))
           mins (map emin (columns ds'))
           maxs (map emax (columns ds'))]
       (fn [v]
