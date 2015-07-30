d599c28edcf5cd659126a990cfc6e5c65e39f3b8
RJ Nowling <rnowling@gmail.com>
2015-07-29 20:49:37 -0500

    Add function for reassigning points to empty clusters.  Update tests to remove conditions for empty clusters

--- test/clatern/kmeans_test.clj
+++ test/clatern/kmeans_test.clj
@@ -12,15 +12,10 @@
           X (into (take 3 (repeat v1))
                     (into (take 3 (repeat v2))
                     (take 3 (repeat v3))))
-          clusters (kmeans X)
+          clusters (kmeans X :num-iters 10)
           centers (mapv #(get % 1) (:centroids clusters))]
       (is (= (count centers) 3))
-      ; we either get: (a) one cluster at each group if
-      ; each cluster is initialized to a separate point
-      ; or (b) a single cluster and two empty clusters if
-      ; clusters are initialized with the equal points
-      (is (or (and (not= -1 (.indexOf centers v1))
-                   (not= -1 (.indexOf centers v2))
-                   (not= -1 (.indexOf centers v3)))
-              (and (not= -1 (.indexOf centers [1/3 1/3 1/3]))
-                   (not= -1 (.indexOf centers []))))))))
+      (is (and (not= -1 (.indexOf centers v1))
+               (not= -1 (.indexOf centers v2))
+               (not= -1 (.indexOf centers v3)))))))
+
